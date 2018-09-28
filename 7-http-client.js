3e4d188845f0327af9aab8c27b4df9571e1fc025
astriskit <harry2office@gmail.com>
2018-09-28 16:44:11 +0530

    added 7-http-client.js

---
+++ 7-http-client.js
@@ -0,0 +1,22 @@
+// Write a program that performs an HTTP GET request to a URL provided to you
+// as the first command - line argument.Write the String contents of each
+// "data" event from the response to a new line on the console(stdout).
+
+function getUrlFromCmdLine() {
+  // gets filename from the command line.
+  return process.argv[2] || "";
+}
+
+function getDataFromUrl(url, onDataCallback) {
+  let { get: httpGet } = require("http");
+  httpGet(url, response => {
+    response.setEncoding("utf8");
+    response.on("data", onDataCallback);
+  });
+}
+
+function httpClient() {
+  getDataFromUrl(getUrlFromCmdLine(), data => console.log(data));
+}
+
+httpClient();
