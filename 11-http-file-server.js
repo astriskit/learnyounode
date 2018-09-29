c57012a04d72bffbf0461066aeace7c99eb68a54
astriskit <harry2office@gmail.com>
2018-09-29 16:36:26 +0530

    added 11-http-file-server.js

---
+++ 11-http-file-server.js
@@ -0,0 +1,35 @@
+// Write an HTTP server that serves the same text file for each request it
+//   receives.
+
+// Your server should listen on the port provided by the first argument to
+// your program.
+
+// You will be provided with the location of the file to serve as the second
+// command - line argument.You must use the fs.createReadStream() method to
+// stream the file contents to the response.
+
+function getPathAndPortFromCmdLine() {
+  // gets port number from the command line.
+  return { path: process.argv[3], port: process.argv[2] };
+}
+
+function getFileStream(filePath) {
+  let { createReadStream } = require("fs");
+  return createReadStream(filePath);
+}
+
+function hostFile(path, port) {
+  let { createServer } = require("http");
+  let server = createServer((request, response) => {
+    let fileStream = getFileStream(path);
+    fileStream.pipe(response);
+  });
+  server.listen(port);
+}
+
+function httpFileServer() {
+  let { path, port } = getPathAndPortFromCmdLine();
+  hostFile(path, port);
+}
+
+httpFileServer();
